d6644119513c7fe7b8277a3890e584bc658ae2f2
Rodrigo Ferreira <rodrigo@treeinova.com>
2022-02-22 17:24:31 -0300

    SqLite

--- sqlite_example/lib/database/database_sqlite.dart
+++ sqlite_example/lib/database/database_sqlite.dart
@@ -1,31 +1,68 @@
+import 'dart:developer';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseSqLite {
   // Future<Database> openConnection() async {
 
-  Future<void> openConnection() async {
+  Future<Database> openConnection() async {
     final databasepath = await getDatabasesPath();
     final databaseFinalPath = join(databasepath, 'SQLITE_EXAMPLE');
 
-    openDatabase(
+    return await openDatabase(
       databaseFinalPath,
       version: 1,
+      onConfigure: (db) async {
+        log("chamado");
+        await db.execute('PRAGMA foreign_keys =ON');
+      },
       // Chamado somente no momento de criação do banco de dados
       // Primeira vez que carrega o aplicativo
       onCreate: (db, version) {
-        print("Chamado");
+        log(" Create Chamado");
         final batch = db.batch();
         batch.execute(
             'create table teste(id Integer primary key autoincrement, nome varchar(200))');
 
-        print(batch);
+        batch.execute(
+            'create table produto(id Integer primary key autoincrement, nome varchar(200))');
+
+        batch.execute(
+            'create table categoria(id Integer primary key autoincrement, nome varchar(200))');
+
+        log(batch.toString());
         batch.commit();
       },
       // Será chamado sempre que ouver uma lateração no version incrementa 1 -> 2
-      onUpgrade: (db, oldVersion, newVersion) {},
+      onUpgrade: (db, oldVersion, newVersion) {
+        log("onUpgrade Chamado");
+        final batch = db.batch();
+        if (oldVersion == 1) {
+          batch.execute(
+              'create table produto(id Integer primary key autoincrement, nome varchar(200))');
+
+          batch.execute(
+              'create table categoria(id Integer primary key autoincrement, nome varchar(200))');
+        }
+
+        if (oldVersion == 2) {
+          batch.execute(
+              'create table categoria(id Integer primary key autoincrement, nome varchar(200))');
+        }
+
+        batch.commit();
+      },
       // Será chamado sempre que ouver uma alteração no version descremental 2 -> 1
-      onDowngrade: (db, oldVersion, newVersion) {},
+      onDowngrade: (db, oldVersion, newVersion) {
+        log("onDowgrade Chamado");
+        final batch = db.batch();
+        if (oldVersion == 3) {
+          batch.execute('drop table categoria');
+        }
+
+        batch.commit();
+      },
     );
   }
 }
--- sqlite_example/lib/home_page.dart
+++ sqlite_example/lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:sqlite_example/database/database_sqlite.dart';
 
@@ -11,8 +13,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   void initState() {
-    DatabaseSqLite().openConnection();
+    _database();
     super.initState();
+  }
+
+  Future<void> _database() async {
+    var database = await DatabaseSqLite().openConnection();
+
+    // database.insert('teste', {'nome': 'Rodrigo Ferreira'});
+    // database.delete('teste', where: 'nome = ?', whereArgs: ['Rodrigo Ferreira']);
+    // database.update('teste', {'nome': 'Rodrigo Lindo'},where: 'nome = ?', whereArgs: ['Rodrigo Ferreira']);
+
+    // var result = await database.query('teste');
+    // log(result.toString());
+
+    // database.rawInsert('insert into teste values(null, ?)', ["jairoEt"]);
+    // database.rawUpdate('update teste set nome = ? where id = ?', ["jairoEtiii", 3]);
+    // database.rawDelete('delete from teste where id = ?', [5]);
+
+    var result = await database.rawQuery('select * from teste');
+    log(result.toList().toString());
   }
 
   @override
